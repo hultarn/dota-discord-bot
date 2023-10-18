9fa8a5844b63cce3a0ca5cdda64da5b6de361594
Jacob Adlers <jacob.adlers@gmail.com>
2023-10-18 22:19:34 +0200

    Stop service before replacing binary

--- deployment/upload-and-restart.sh
+++ deployment/upload-and-restart.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
 # Exit on any error
-set -ex
+set -e
 
 mkdir "$HOME/.ssh"
 echo "$AWS_KEY" > "$HOME/.ssh/aws_key"
 chmod 400 "$HOME/.ssh/aws_key"
 echo "ec2-51-20-63-178.eu-north-1.compute.amazonaws.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPizi++gnfLgkIW3qHSViUeaUXTgtk9RVzwpzPQUXMU/7CF3+i/08WHdKhWfXlSyrAt1u5oGFGxsv2Wgoyvc3+Y=" > "$HOME/.ssh/known_hosts"
 echo "$ENV" > .env
+
+# Show commands as they're executed
+set -x
 scp -i "$HOME/.ssh/aws_key" .env "$EC2_INSTANCE"
+ssh -i "$HOME/.ssh/aws_key" "$EC2_INSTANCE" sudo systemctl stop dbot-signup
+ssh -i "$HOME/.ssh/aws_key" "$EC2_INSTANCE" sudo systemctl stop dbot-league
+sleep 2 # some downtime
 scp -i "$HOME/.ssh/aws_key" dbot "$EC2_INSTANCE":/home/ubuntu/dbot
-ssh -i "$HOME/.ssh/aws_key" "$EC2_INSTANCE" sudo systemctl restart dbot-signup
-ssh -i "$HOME/.ssh/aws_key" "$EC2_INSTANCE" sudo systemctl restart dbot-league
+sleep 2
+ssh -i "$HOME/.ssh/aws_key" "$EC2_INSTANCE" sudo systemctl start dbot-signup
+ssh -i "$HOME/.ssh/aws_key" "$EC2_INSTANCE" sudo systemctl start dbot-league
